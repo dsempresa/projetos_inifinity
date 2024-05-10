eb0c864292895ace65ca20120bae2f440b6b9433
dsempresa <130731804+dsempresa@users.noreply.github.com>
2024-05-09 21:04:13 -0300

    ajustes

--- projeto_cripton/main.js
+++ projeto_cripton/main.js
@@ -1,11 +1,9 @@
-// Dados de exemplo das criptomoedas
+
 const cryptos = [
     { name: "Bitcoin", logo: "./img/bitcoin-icon-sign-payment-symbol-600nw-1938997753.webp", price: 35000, date: "2024-05-09", description: "A primeira criptomoeda descentralizada." },
     { name: "Ethereum", logo: "./img/images_ethereum.png", price: 2500, date: "2024-05-09", description: "Uma plataforma para contratos inteligentes e aplicativos descentralizados." },
-    // Adicione mais criptomoedas conforme necessário
   ];
   
-  // Função para exibir as criptomoedas na página
   function displayCryptos() {
     const cryptoCardsContainer = document.getElementById("cryptoCardsContainer");
     cryptoCardsContainer.innerHTML = "";
@@ -23,30 +21,23 @@ const cryptos = [
     });
   }
   
-  // Função para filtrar as criptomoedas conforme o texto digitado no campo de filtro
   document.getElementById("cryptoFilter").addEventListener("input", function() {
     const filterValue = this.value.toLowerCase();
     const filteredCryptos = cryptos.filter(crypto => crypto.name.toLowerCase().includes(filterValue));
     displayCryptos(filteredCryptos);
   });
   
-  // Exibir as criptomoedas quando a página carregar
   displayCryptos();
   
-// Taxas de conversão de exemplo
 const conversionRates = {
     bitcoin: {
       real: 35000,
-      // Adicione mais taxas de conversão conforme necessário
     },
     ethereum: {
       real: 2500,
-      // Adicione mais taxas de conversão conforme necessário
     },
-    // Adicione mais criptomoedas conforme necessário
   };
   
-  // Função para realizar a conversão de moeda
   function convertCurrency() {
     const cryptoAmount = parseFloat(document.getElementById("cryptoAmount").value);
     const fromCrypto = document.getElementById("fromCrypto").value;
@@ -58,6 +49,5 @@ const conversionRates = {
     document.getElementById("conversionResult").textContent = `${cryptoAmount} ${fromCrypto.toUpperCase()} = ${convertedAmount.toFixed(2)} ${toCrypto.toUpperCase()}`;
   }
   
-  // Adicionar um listener de evento ao botão de conversão
   document.getElementById("convertButton").addEventListener("click", convertCurrency);
   
